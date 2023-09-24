d258b82975a2678138021697ab75021baef831ea
badrxd <badrxdxd@gmail.com>
2023-09-24 23:44:31 +0100

    script make changes to configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,11 @@
 # make changes in SSH configuration file
 
+file_line { 'Declare the identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  }
 
-file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/badr',
+file_line { 'Turn off the passwd':
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
   }
--- 0x0B-ssh/100-puppet_ssh_config1.pp
+++
@@ -1,8 +0,0 @@
-# make changes in SSH configuration file
-
-
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/badr',
-  line   => '    PasswordAuthentication no',
-  }

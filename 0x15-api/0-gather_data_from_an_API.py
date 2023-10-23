c1dff7cc11b0d51ce6c316b682df2472573d0005
badrxd <badrxdxd@gmail.com>
2023-10-23 20:12:46 +0100

    task 0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress. """
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    """ """
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    # user = '{}users/{}'.format(base_url, argv[1])
+
+    EMPLOYEE_NAME = requests.get(
+        '{}users/{}'.format(base_url, argv[1])).json()
+    NUMBER_OF_DONE_TASKS = requests.get(
+        '{}todos?userId={}&completed=true'.format(base_url,
+                                                  argv[1])).json()
+    TOTAL_NUMBER_OF_TASKS = requests.get(
+        '{}todos?userId={}'.format(base_url, argv[1])).json()
+
+    print(
+        'Employee {} is done with tasks({}/{}):\
+            '.format(EMPLOYEE_NAME['name'], len(NUMBER_OF_DONE_TASKS),
+                     len(TOTAL_NUMBER_OF_TASKS)))
+
+    for task in NUMBER_OF_DONE_TASKS:
+        print('\t'+task['title'])

79c1645b221ef939dee061b59c314e73f6b65aef
badrxd <badrxdxd@gmail.com>
2023-10-23 23:50:41 +0100

    task 1. Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,8 +12,10 @@ if __name__ == '__main__':
     base_url = 'https://jsonplaceholder.typicode.com/'
     EMPLOYEE_NAME = requests.get(
         '{}users/{}'.format(base_url, argv[1])).json()
+
     TOTAL_NUMBER_OF_TASKS = requests.get(
         '{}todos?userId={}'.format(base_url, argv[1])).json()
+
     file = '{}.csv'.format(argv[1])
     with open(file, 'w', newline='') as csvfile:
         fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
@@ -22,6 +24,6 @@ if __name__ == '__main__':
                                 quoting=csv.QUOTE_ALL)
         for task in TOTAL_NUMBER_OF_TASKS:
             writer.writerow({"USER_ID": argv[1],
-                             'USERNAME': EMPLOYEE_NAME['name'],
-                             'TASK_COMPLETED_STATUS': task['completed'],
-                             'TASK_TITLE': task['title']})
+                             'USERNAME': EMPLOYEE_NAME.get("username"),
+                             'TASK_COMPLETED_STATUS': task.get('completed'),
+                             'TASK_TITLE': task.get('title')})

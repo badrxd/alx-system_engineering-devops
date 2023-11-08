b7bf99e681da7e18eb8fb5f466b8affc94fef5f8
badrxd <badrxdxd@gmail.com>
2023-11-08 19:56:15 +0100

    fix task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def count_words(subreddit, word_list, Dict={}, after=None):
     """send get http req to Reddit API and returns 10 hot posts titels"""
 
     if subreddit is None:
@@ -16,13 +16,20 @@ def recurse(subreddit, hot_list=[], after=None):
     if data.status_code != 200:
         return None
 
-    titels = data.json()["data"]["children"]
-    for title in titels:
-        hot_list.append(title.get('data').get('title'))
-
     the_after = data.json()["data"]["after"]
 
     if not the_after:
-        return hot_list
+        Dict = sorted(
+            Dict.items(), key=lambda item: (-item[1], item[0].lower()))
+        for key, value in Dict:
+            print('{}: {}'.format(key, value))
+        return
     else:
-        return recurse(subreddit, hot_list, after=the_after)
+        titels = data.json()["data"]["children"]
+        for title in titels:
+            for word in word_list:
+                if word in title.get('data').get('title').lower():
+                    if word not in Dict:
+                        Dict[word] = 0
+                    Dict[word] = Dict[word]+1
+        return count_words(subreddit, word_list, Dict, after=the_after)
